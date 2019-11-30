29f4eb52b92c76a3667147ce48f1397de2e1e8b5
Xisifer <xisifer@gmail.com>
2019-11-30 14:58:06 -0600

    Hooked up react-table. Tried to start setting custom columns. Ran into problems pointing the accessors correctly.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import data from './data/weapons.json'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React AS A DUMB FACE
-        </a>
-      </header>
-    </div>
-  );
+import ReactTable from 'react-table'
+import 'react-table/react-table.css'
+
+
+
+class App extends Component {
+
+  render() {
+    // const data = [
+    //   {
+    //     name: 'Tanner Linsley',
+    //     age: 26,
+    //     friend: {
+    //       name: 'Jason Maurer',
+    //       age: 23,
+    //     }
+    //   } 
+      // {
+      //   name: "Hammer",
+      //   weight: "2",
+      //   description: "This is a hammer."
+      // }
+    // ]
+    console.log(data);
+    const columns = [{
+      Header: 'Name',
+      accessor: 'name' // String-based value accessors!
+    }, {
+      Header: 'Category',
+      accessor: data.Weapon[0].Categories.Category[0],
+      Cell: props => <span className='number'>{props.value}</span> // Custom cell components!
+    }, {
+      id: 'friendName', // Required because our accessor is not a string
+      Header: 'Damage',
+      accessor: data.Weapon[0].DamageAdd
+      // accessor: d => d.friend.name // Custom value accessors!
+    }, {
+      Header: props => <span>Friend Age</span>, // Custom header components!
+      accessor: 'friend.age'
+    }]
+  
+    return (
+      <ReactTable
+        data={data}
+        columns={columns}
+      />
+    )
+  }
 }
 
 export default App;
